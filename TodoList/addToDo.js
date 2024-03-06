c0cbd310cb842b656e2a31d83001ff4955b0e576
codeloverfront-end <chakibgherras@yahoo.fr>
2024-03-06 16:24:29 +0100

    Added new files

--- TodoList/addToDo.js
+++ TodoList/addToDo.js
@@ -1,24 +1,46 @@
 // 1 / Ajouter une todo via un formulaire
 
-// Using DOM
-function addToDo() {
-  let titleHTML = document.querySelector("#title");
-  let descriptionHTML = document.querySelector("#description");
-  let dueDateHTML = document.querySelector("#date");
-  let notUrgentHTML = document.querySelector("#NU");
-  let normalHTML = document.querySelector("#N");
-  let urgentHTML = document.querySelector("#U");
-
-  // Making sure that the form isn't empty
-  if (
-    titleHTML.value === "" &&
-    descriptionHTML.value === "" &&
-    dueDateHTML.value === "" &&
-    (!notUrgentHTML.checked || !normalHTML.checked || !urgentHTML.checked)
+class addToDo {
+  constructor(
+    titleHTML,
+    descriptionHTML,
+    dueDateHTML,
+    notUrgentHTML,
+    normalHTML,
+    urgentHTML
   ) {
-    console.log("Your form isn't complete");
-  } else {
-    console.log("Your form is complete");
+    this.titleHTML = titleHTML;
+    this.descriptionHTML = descriptionHTML;
+    this.dueDateHTML = dueDateHTML;
+    this.notUrgentHTML = notUrgentHTML;
+    this.normalHTML = normalHTML;
+    this.urgentHTML = urgentHTML;
+  }
+
+  // Using DOM
+  targetHTML() {
+    this.titleHTML = document.querySelector("#title");
+    this.descriptionHTML = document.querySelector("#description");
+    this.dueDateHTML = document.querySelector("#date");
+    this.notUrgentHTML = document.querySelector("#NU");
+    this.normalHTML = document.querySelector("#N");
+    this.urgentHTML = document.querySelector("#U");
+  }
+  checkForm() {
+    // Making sure that the form isn't empty
+    this.targetHTML();
+    if (
+      this.titleHTML.value === "" &&
+      this.descriptionHTML.value === "" &&
+      this.dueDateHTML.value === "" &&
+      (!this.notUrgentHTML.checked ||
+        !this.normalHTML.checked ||
+        !this.urgentHTML.checked)
+    ) {
+      console.log("Your form isn't complete");
+    } else {
+      console.log("Your form is complete");
+    }
   }
 }
 export default addToDo;
--- TodoList/saveToDo.js
+++ TodoList/saveToDo.js
@@ -1,1 +1,9 @@
 // 5 / Sauvegarder les todos au sein du local Storage
+import addToDo from "addToDo.js";
+// Target the form
+let formHTML = document.querySelector("form");
+
+// If my form is submitted, then storage it
+
+//
+let form = {};
